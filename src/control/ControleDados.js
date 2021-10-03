61cec6e0ce02d733e3b30db950fcb584dcb6e5a3
Ruulul <vamjunior01@gmail.com>
2021-10-03 10:55:36 -0300

    package-lock adicionado, removido logs

--- src/control/ControleDados.js
+++ src/control/ControleDados.js
@@ -1,9 +1,7 @@
 export function addDado(Entrada, Posicao=undefined) { // Entrada = {nome:"nome", valor:15.20, data:Date("03/05")}
    var Entradas = getDados();
-   console.log("addDado1:" + JSON.stringify(Entradas))
    if (Posicao === undefined) Posicao = Object.keys(Entradas).length
    Entradas[Posicao] = Entrada;
-   console.log("addDado2:" + JSON.stringify(Entradas))
    localStorage.setItem("Entradas", JSON.stringify(Entradas));
 }
 
@@ -14,10 +12,8 @@ export function getDados() {
 }
 
 export function removeDados(Posicao) {
-    console.log("removeDados1:" + JSON.stringify(Object.values(getDados())))
     var Entradas = Object.values(getDados());
     Entradas.splice(Posicao, 1)
-    console.log("RemoveDados2:" + JSON.stringify(Entradas))
     setDados(Object.assign({}, Entradas))
 }
 
